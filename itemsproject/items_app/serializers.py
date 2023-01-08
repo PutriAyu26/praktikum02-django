b646ec83688fd5ca6a2fc95d00264a41bd8f1134
PutriAyu26 <putriwicaksono26@gmail.com>
2023-01-08 18:45:52 +0700

    coding kedua

---
+++ itemsproject/items_app/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from items_app.models import ItemsModel
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemsModel
+        fields = ['name', 'price']
